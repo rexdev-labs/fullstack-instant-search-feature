0e8b06f91b2fc2c15812f37562829c9f2a177d42
Rex-BlockChainSpace <63227295+rexdev-labs@users.noreply.github.com>
2022-11-24 15:52:07 +0800

    add useEffect on projet

--- search-box/src/App.jsx
+++ search-box/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Animals from './Animals'
 
-function App() {
+
+function useAnimals() {
   const [animal, setAnimals] = useState([])
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery)
+  }, []);
 
   const search = async (q) => {
     const response = await fetch(
@@ -13,13 +19,19 @@ function App() {
 
     const data = await response.json();
     setAnimals(data)
-  }
 
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return {search, animal}
+}
+
+function App() {
+  const{search, animal} = useAnimals();
   return (
     <div className="App">
       <h1>Search Box App</h1>
       <input type="text" placeholder="Search" onChange={(e) => search(e.target.value)} />
-
       <ul>
         {animal.map((animal) => (
           <Animals key={animal.id} {...animal} />
